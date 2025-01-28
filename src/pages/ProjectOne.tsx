a5da48b067c276b31ca47302ec474adc1a50783c
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2025-01-28 11:51:16 -0500

    almost finished project pages

--- src/pages/ProjectOne.tsx
+++ src/pages/ProjectOne.tsx
@@ -35,9 +35,30 @@ function ProjectOne() {
               </Col>
             </Row>
           </div>
-          <div className="text-center">
-            <h3>This Project was </h3>
+          <div className="mt-3">
+            <h5>It was an absolute pleasure to work on this project, which involved creating a fully functional and visually engaging website for an up-and-coming band that has garnered significant attention with over 1 million streams on their chart-topping hit, "Lonely Road". The band approached me seeking a complete overhaul of their outdated website. They were eager for a fresh, modern design that would captivate their audience and reflect the dynamic energy of their music.</h5>
+            <h5 className="mt-3">To meet their needs, I utilized a combination of TypeScript and React to develop a sleek and user-friendly platform. The goal was to deliver a visually striking and responsive website that not only showcased their artistic identity but also provided an enhanced user experience. This project required careful attention to detail and was a joy to work on.</h5>
           </div>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col>
+          <img
+            src="/images/MaypoBio.png"
+            alt="Project 1 Bio Page"
+            className="img-fluid"
+            style={{ width: '100%', height: 'auto' }}
+          />
+          <h5 style={{ marginTop: '30px' }}>Maypo Deluxe Biography</h5>
+        </Col>
+        <Col>
+          <img
+            src="/images/MaypoSongsPage.png"
+            alt="Project 1 Songs Page"
+            className="img-fluid"
+            style={{ width: '100%', height: 'auto' }}
+          />
+          <h5 style={{ marginTop: '30px', marginBottom: '50px' }}>Maypo Deluxe Songs Page</h5>
         </Col>
       </Row>
     </Container>
--- src/pages/ProjectTwo.tsx
+++ src/pages/ProjectTwo.tsx
@@ -1,27 +1,63 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 function ProjectTwo() {
   return (
-    <Container className="mt-5">
+    <Container className="mt-3">
       <Row>
         <Col>
+          <div className="text-center">
+            <h1 style={{ fontSize: '4rem' }}>Project 2</h1>
+          </div>
           <img
             src="/images/FirstStepMain.png"
             alt="Project 2 Main Image"
-            className="img-fluid"
+            className="img-fluid mt-5"
           />
           <h1 style={{ fontSize: '6rem', marginTop: '50px', fontWeight: '400' }}>FirstStep Fitness</h1>
           <div className="links-container" style={{ marginTop: '65px' }}>
-            <p>
-              Github Link: <a href="https://github.com/RomanScandariato/FirstStep_Fitness" target="_blank" rel="noopener noreferrer">https://github.com/RomanScandariato/FirstStep_Fitness</a>
-            </p>
-            <p>
-              Website Link: <a href="https://firststep-fitness.onrender.com/" target="_blank" rel="noopener noreferrer">https://firststep-fitness.onrender.com/</a>
-            </p>
-            <p>
-            Flourishing Band with needs for website development to further push music and spread the love and joy of what they love!
-            </p>
+            <Row>
+              <Col>
+                <a href="https://github.com/RomanScandariato/FirstStep_Fitness" target="_blank" rel="noopener noreferrer" className="no-underline" style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                  <FontAwesomeIcon icon={faGithub} size="3x" style={{ color: 'white', marginRight: '20px' }} />
+                  <h2 style={{ fontSize: '1.5rem', color: 'white' }}>github.com/FirstStep_Fitness</h2>
+                </a>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <a href="https://firststep-fitness.onrender.com" target="_blank" rel="noopener noreferrer" className="no-underline" style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                  <FontAwesomeIcon icon={faGlobe} size="3x" style={{ color: 'white', marginRight: '20px' }} />
+                  <h2 style={{ fontSize: '1.5rem', color: 'white' }}>firststep-fitness.onrender.com</h2>
+                </a>
+              </Col>
+            </Row>
           </div>
+          <div className="mt-3">
+            <h5>This was a group project including me and three other developers. This project was a very intense and difficult project with all the funtionality that was needed for it to work properly. The app was designed to inspire users to try new exercises and keep track of the exercises that work for them. After creating a free account, the user will be able to look up exercises by the muscle they are targeting for the workout, save the exercises they like most, update them if needed, and delete the more entry level exercises as they gain experience. This was a difficult website to make when it came to the funtionality but was super rewarding to work on. The stack we used was Typescript, React, Node.js, and MongoDB. It was such a challenge but was worth it in the end.</h5>
+          </div>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col>
+          <img
+            src="/images/WorkoutPlan.png"
+            alt="Project 2 Workout Plan Page"
+            className="img-fluid"
+            style={{ width: '100%', height: 'auto' }}
+          />
+          <h5 style={{ marginTop: '30px' }}>Workout Plan Page</h5>
+        </Col>
+        <Col>
+          <img
+            src="/images/AddExercise.png"
+            alt="Project 2 Add Exercise Page"
+            className="img-fluid"
+            style={{ width: '100%', height: 'auto' }}
+          />
+          <h5 style={{ marginTop: '30px', marginBottom: '50px' }}>Add Exercise Page</h5>
         </Col>
       </Row>
     </Container>
