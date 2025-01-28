c75fe2f9c573dc625f252680675279393d995a5d
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2025-01-28 01:44:43 -0500

    starting fixing project pages

--- src/pages/ProjectOne.tsx
+++ src/pages/ProjectOne.tsx
@@ -1,26 +1,42 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 function ProjectOne() {
   return (
-    <Container className="mt-5">
+    <Container className="mt-3">
       <Row>
         <Col>
+          <div className="text-center">
+            <h1 style={{ fontSize: '4rem' }}>Project 1</h1>
+          </div>
           <img
             src="/images/MaypoDeluxeMain.png"
             alt="Project 1 Main Image"
-            className="img-fluid"
+            className="img-fluid mt-5"
           />
           <h1 style={{ fontSize: '6rem', marginTop: '50px', fontWeight: '400' }}>Maypo Deluxe</h1>
           <div className="links-container" style={{ marginTop: '65px' }}>
-            <p>
-              Github Link: <a href="https://github.com/RomanScandariato/Maypo_Deluxe_Website" target="_blank" rel="noopener noreferrer">https://github.com/RomanScandariato/Maypo_Deluxe_Website</a>
-            </p>
-            <p>
-              Website Link: <a href="https://maypodeluxe.com" target="_blank" rel="noopener noreferrer">https://maypodeluxe.com</a>
-            </p>
-            <p>
-            Flourishing Band with needs for website development to further push music and spread the love and joy of what they love!
-            </p>
+            <Row>
+              <Col>
+                <a href="https://github.com/RomanScandariato/Maypo_Deluxe_Website" target="_blank" rel="noopener noreferrer" className="no-underline" style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                  <FontAwesomeIcon icon={faGithub} size="3x" style={{ color: 'white', marginRight: '20px' }} />
+                  <h2 style={{ fontSize: '1.5rem', color: 'white' }}>github.com/Maypo_Deluxe_Website</h2>
+                </a>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <a href="https://maypodeluxe.com" target="_blank" rel="noopener noreferrer" className="no-underline" style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                  <FontAwesomeIcon icon={faGlobe} size="3x" style={{ color: 'white', marginRight: '20px' }} />
+                  <h2 style={{ fontSize: '1.5rem', color: 'white' }}>maypodeluxe.com</h2>
+                </a>
+              </Col>
+            </Row>
+          </div>
+          <div className="text-center">
+            <h3>This Project was </h3>
           </div>
         </Col>
       </Row>
